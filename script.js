d139526d9d9fa1c0d16adf03f2ea1047b88219bf
Uhaha123 <markeliezon12@gmail.com>
2024-02-20 18:39:50 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function myVideo() {
+    document.getElementById('myVideo').load();
+    
+    var video = document.getElementById('myVideo');
+    var button = document.getElementById('btn');
+    
+    if (video.paused || video.ended) {
+        video.play();
+        button.innerText = "Pause";
+        button.disabled = true;
+        document.getElementById('stop').style.display="block";
+        video.loop = true;
+    } else {
+        video.pause();
+        button.innerText = "Play Again";
+    }
+   
+}
+
